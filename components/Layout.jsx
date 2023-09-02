83bb0b56cfa79dbacbb364c899044c15d010ea95
Ayush <ayush.krsh9@gmail.com>
2023-09-02 19:09:02 +0530

    Deployment issue resolved

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Zanime Demo</title>
       </Head>
       <header>
         <Navbar />
